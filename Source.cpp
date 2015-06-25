b7d85180bef2b4007a8b674c434025b1cf2c6eea
Diego Perez <dperez@m5-software.com>
2015-06-24 20:57:18 -0430

    producto terminado freelancer

--- Source.cpp
+++ Source.cpp
@@ -4,14 +4,18 @@
 #include <string>
 using namespace std;
 
-//clase para representar las ciudades
+//clase para representar las ubicaciones
 class Ubicacion
 {
 	public:
 		string nombre;
 		//int num;
 		vector <string> rutas;
-		//bool visitado;
+		bool visitado;
+	Ubicacion()
+	{
+		visitado = false;
+	}
 };
 
 //clase para almacenar la informacion de los caminos entre las ciudades
@@ -23,11 +27,11 @@ class Camino
 };
 
 
-int cn = 9; //cantidad de ciudades
-int rutaOrigen =-1; // las rutas estan representadas desde el 0 al 8 ya que son 9 rutas.
-vector<vector <int>> distancias; // matriz de distancias que se encontraban en excel
+int cn = 15; //cantidad de ubicaciones
+int ubicacionOrigen =-1;
+int ubicacionDestino =-1;
 vector<Camino> caminos; // vector donde se almacenan todos los caminos posibles para luego elegir el optimo
-vector<Ubicacion> Ubicaciones; // arreglo de las 9 ciudades especificadas
+Ubicacion ubicaciones[15]; // arreglo de las 15 ubicaciones especificadas
 
 //inicializa la informacion de la ciudades y sus interconexiones
 void inicializarUbicaciones()
@@ -37,7 +41,8 @@ void inicializarUbicaciones()
 	Ubicacion p1;
 	p1.nombre = "Siete Sur";
 	string p1rutas[] = {
-		"103"
+		"6"
+		,"103"
 		, "106"
 		, "110"
 		, "112"
@@ -50,7 +55,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp1(std::begin(p1rutas), std::end(p1rutas));
 	p1.rutas = vp1;
-	Ubicaciones.push_back(p1);
+	ubicaciones[0] = p1;
 	/*End*/
 
 	/* BEGIN */
@@ -71,7 +76,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp2(std::begin(p2rutas), std::end(p2rutas));
 	p2.rutas = vp2;
-	Ubicaciones.push_back(p2);
+	ubicaciones[1] = p2;
 	/*End*/
 
 	/* BEGIN */
@@ -91,7 +96,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp3(std::begin(p3rutas), std::end(p3rutas));
 	p3.rutas = vp3;
-	Ubicaciones.push_back(p3);
+	ubicaciones[2] = p3;
 	/*End*/
 
 	/* BEGIN */
@@ -107,7 +112,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp4(std::begin(p4rutas), std::end(p4rutas));
 	p4.rutas = vp4;
-	Ubicaciones.push_back(p4);
+	ubicaciones[3] = p4;
 	/*End*/
 
 	/* BEGIN */
@@ -125,7 +130,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp5(std::begin(p5rutas), std::end(p5rutas));
 	p5.rutas = vp5;
-	Ubicaciones.push_back(p5);
+	ubicaciones[4] = p5;
 	/*End*/
 
 	/* BEGIN */
@@ -147,7 +152,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp6(std::begin(p6rutas), std::end(p6rutas));
 	p6.rutas = vp6;
-	Ubicaciones.push_back(p6);
+	ubicaciones[5] = p6;
 	/*End*/
 
 	/* BEGIN */
@@ -165,7 +170,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp7(std::begin(p7rutas), std::end(p7rutas));
 	p7.rutas = vp7;
-	Ubicaciones.push_back(p7);
+	ubicaciones[6] = p7;
 	/*End*/
 
 	/* BEGIN */
@@ -179,7 +184,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp8(std::begin(p8rutas), std::end(p8rutas));
 	p8.rutas = vp8;
-	Ubicaciones.push_back(p8);
+	ubicaciones[7] = p8;
 	/*End*/
 
 	/* BEGIN */
@@ -192,7 +197,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp9(std::begin(p9rutas), std::end(p9rutas));
 	p9.rutas = vp9;
-	Ubicaciones.push_back(p9);
+	ubicaciones[8] = p9;
 	/*End*/
 
 	/* BEGIN */
@@ -205,7 +210,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp10(std::begin(p10rutas), std::end(p10rutas));
 	p10.rutas = vp10;
-	Ubicaciones.push_back(p10);
+	ubicaciones[9] = p10;
 	/*End*/
 
 	/* BEGIN */
@@ -216,7 +221,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp11(std::begin(p11rutas), std::end(p11rutas));
 	p11.rutas = vp11;
-	Ubicaciones.push_back(p11);
+	ubicaciones[10] = p11;
 	/*End*/
 
 	/* BEGIN */
@@ -233,7 +238,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp12(std::begin(p12rutas), std::end(p12rutas));
 	p12.rutas = vp12;
-	Ubicaciones.push_back(p12);
+	ubicaciones[11] = p12;
 	/*End*/
 
 	/* BEGIN */
@@ -248,7 +253,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp13(std::begin(p13rutas), std::end(p13rutas));
 	p13.rutas = vp13;
-	Ubicaciones.push_back(p13);
+	ubicaciones[12] = p13;
 	/*End*/
 
 	/* BEGIN */
@@ -271,7 +276,7 @@ void inicializarUbicaciones()
 
 	vector<string> vp14(std::begin(p14rutas), std::end(p14rutas));
 	p14.rutas = vp14;
-	Ubicaciones.push_back(p14);
+	ubicaciones[13] = p14;
 	/*End*/
 
 	/* BEGIN */
@@ -294,17 +299,139 @@ void inicializarUbicaciones()
 
 	vector<string> vp15(std::begin(p15rutas), std::end(p15rutas));
 	p15.rutas = vp15;
-	Ubicaciones.push_back(p15);
+	ubicaciones[14] = p15;
 	/*End*/
 
 }
 
+//guarda un posible camino como el mas corto en el vector de caminos
+void guardarCamino(string camino, int distanciaTotal)
+{
+	Camino caminoAGuardar;
+	caminoAGuardar.camino = camino;
+	caminoAGuardar.distanciaTotal = distanciaTotal;
+
+	caminos.push_back(caminoAGuardar);
+}
+//funcion recursiva que encuentra el camino mas corto entre la ciudad origen y destino
+void buscarCaminoMasCorto(int a/*origen*/, int z/*destino*/, int p/*ultima ubicaiones consultada*/,int distanciaTotal, string camino)
+{
+	Ubicacion* actual = &ubicaciones[a];
+	Ubicacion* destino = &ubicaciones[z];
+	actual -> visitado = true;
+	if(a == z)
+	{
+		guardarCamino(camino, distanciaTotal);
+		return;
+	}
+	
+	for(int x = p; x < cn; x ++)
+	{
+		if(x != a)
+		{
+			Ubicacion* posibleUbicacion = &ubicaciones[x];
+			for(unsigned int i=0; i < actual->rutas.size();i++)
+			{
+				for(unsigned int j=0; j < posibleUbicacion->rutas.size();j++)
+				{
+					if(actual->rutas[i] == posibleUbicacion->rutas[j]
+						&& posibleUbicacion->visitado == false)
+					{
+						string caminoBackUp = camino;
+						camino += "En (" + actual->nombre + ") tomar ruta " + actual->rutas[i] + " hasta (" + posibleUbicacion->nombre + "),";
+						buscarCaminoMasCorto(x,z,x,distanciaTotal+1, camino);
+						camino = caminoBackUp;
+					}
+				}
+			}
+			posibleUbicacion -> visitado = false;
+		}
+	}
+}
+
+
+// imprime el camino mas corto
+void imprimirElCaminoMasCorto()
+{
+	int elMasCorto;
+	int minimo = 99999;
+	for(unsigned int i =0; i< caminos.size(); i++)
+	{
+		if(caminos[i].distanciaTotal < minimo)
+		{
+			elMasCorto = i;
+			minimo = caminos[i].distanciaTotal;
+		}
+	}
+	cout << "Las instrucciones a seguir para llegar de manera eficiente al destino son: "<< caminos[elMasCorto].camino
+			<< " y el numero de transbordos seran " <<caminos[elMasCorto].distanciaTotal-1<<endl; 
+}
+//lee la ubicacion origen especificada por el usuario
+void leerUbicacionInicio()
+{
+	cout << "Por favor, introduzca el numero de la ubicacion origen: ";
+	cin >> ubicacionOrigen;
+	ubicacionOrigen--;
+}
+//lee la ubicacion destino especificada por el usuario
+void leerUbicacionDestino()
+{
+	cout << "Por favor, introduzca el numero de la ubicacion destino: ";
+	cin >> ubicacionDestino;
+	ubicacionDestino--;
+}
+//imprime las rutas del punto origen
+void imprimirRutasPuntoOrigen(int a)
+{
+	Ubicacion* ubicacion = &ubicaciones[a];
+	cout << "Las rutas disponibles para el punto origen(" << ubicacion->nombre <<") son las siguientes: " <<endl;
+	for(unsigned int i = 0; i < ubicacion -> rutas.size();i++)
+	{
+		cout << ubicacion -> rutas[i] << ", ";
+	}
+	cout << endl;
+}
+//imprime las rutas del punto destino
+void imprimirRutasPuntoDestino(int a)
+{
+	Ubicacion* ubicacion = &ubicaciones[a];
+	cout << "Las rutas disponibles para el punto destino(" << ubicacion->nombre <<") son las siguientes: " <<endl;
+	for(unsigned int i = 0; i < ubicacion -> rutas.size();i++)
+	{
+		cout << ubicacion -> rutas[i] << ", ";
+	}
+	cout << endl;
+}
 //funcion principal del programa
 int main()
 {
-
 	inicializarUbicaciones();
+	cout << "Bienvenido a Viaje eficiente, las ubicaciones disponibles son las siguientes:" << endl;
+	cout << endl;
+	for(int i=0; i< cn; i++)
+	{
+		cout << i+1 << ". " << ubicaciones[i].nombre<< ". "<< endl;
+	}
+	cout << endl;
+	leerUbicacionInicio();
+	cout << endl;
+	leerUbicacionDestino();
+	cout << endl;
 
-
-	system("pause");
+	if((ubicacionOrigen >=0 && ubicacionOrigen<=14)
+		&& (ubicacionDestino >=0 && ubicacionDestino<=14))
+	{
+		buscarCaminoMasCorto(ubicacionOrigen, ubicacionDestino,0, 0, "");
+		cout << endl;
+		imprimirRutasPuntoOrigen(ubicacionOrigen);
+		cout << endl;
+		imprimirRutasPuntoDestino(ubicacionDestino);
+		cout << endl;
+		imprimirElCaminoMasCorto();
+	}else
+	{
+		cout << endl;
+		cout << "Se ha producido un error, por favor introduzca algun valor correspondiente al menu." << endl;
+	}
+	//system("pause");
 }
